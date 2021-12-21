9b9e0bbdf8e5375a8be93d19c9a5a5a8fce748f6
boolean <y133@163.com>
2021-12-21 21:05:02 +0800

    发布

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  base: 'https://mtttm.github.io/vue-keyboard-cn-doc/',
+  base: '/vue-keyboard-cn-doc/',
   title: 'vue-key-board-cn',
   description: '手机端输入法模拟插件，中文输入法，英文输入法，数字输入法',
   themeConfig: {
